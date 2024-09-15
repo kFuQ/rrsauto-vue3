1886e529d2d7ea40b84bfd25a993035615c7f073
kFuQ <kfuq@kfuq.net>
2024-09-14 19:29:25 -0700

    fixed layout issue

--- src/components/sections/IntroMain.vue
+++ src/components/sections/IntroMain.vue
@@ -12,9 +12,9 @@
           convenience. And remember, we want to keep you in our loyal customer
           family.
           <strong
-            >If you don’t see the service you’re looking for, just ask!</strong
+            >If you don't see the service you're looking for, just ask!</strong
           ><br /><br />
-          Our experienced technicians can definitely handle your vehicle’s
+          Our experienced technicians can definitely handle your vehicle's
           problem. <a href="/contact">Contact us</a> via phone, email or
           <a
             href="https://maps.app.goo.gl/hpqARwi6HrWtv4ct8"
@@ -23,15 +23,15 @@
             >in person</a
           >
           for more information about the kinds of different services that we
-          offer.<br/><br/><br/>
+          offer.<br/>
         </p>
       </v-responsive>
 
-      <v-row class="pt-20">
+      <v-row class="pt-20 ">
         <v-col v-for="card in cards" :key="card.title" cols="12" md="4" class="card-spacing">
           <v-row no-gutters>
             <v-col :cols="card.callout ? 9 : 12">
-              <div class="pr-2 mt-3">
+              <div class="pr-2">
                 <div class="text--disabled" v-text="card.subtitle"></div>
                 <h4
                   class="text-uppercase mt-1 mb-3"
@@ -93,6 +93,7 @@ export default {
 <style scoped>
 .intro-main {
   width: 80%;
+  height: 100%;
   margin: 0 auto; /* Center the component */
 }
 .card-spacing {
